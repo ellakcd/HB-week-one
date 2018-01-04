12ef44b0bf9c399c5c4a6e2b6dc4dd7e54d72d7b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 12:34:39 -0800

    Created add_mult and add_cubes functions

--- arithmetic.py
+++ arithmetic.py
@@ -22,12 +22,12 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
-    return num1 / num2
+    return float(num1) / num2
 
 
 def square(num1):
     """Return the square of the input."""
-    
+
     return num1 ** 2
 
 
@@ -48,6 +48,16 @@ def mod(num1, num2):
 
     return num1 % num2
 
+def add_mult(num1, num2, num3):
+    """Adds the first two and multiples sum with third"""
+
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Cubes two numbers and returns the sum of the cubes"""
+
+    return num1 ** 3 + num2 ** 3
+
 print add(1, 2)
 print subtract(10, 5)
 print multiply(3, 4)
@@ -56,3 +66,5 @@ print square(5)
 print cube(2)
 print power(3, 2)
 print mod(10, 3)
+print add_mult(1, 2, 3)
+print add_cubes(2, 3)
